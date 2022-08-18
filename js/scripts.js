8327efcca7449ad9970080cd6638951f583d4873
Robert Onstott <roonstott@gmail.com>
2022-08-17 20:31:38 -0700

    create calculator functionality with JS

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+//business logic
+function add(number1, number2) {
+  return (number1 + number2);
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// user interface logic
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+let addAns = add(number1, number2);
+let subtractAns = subtract(number1, number2);
+let multiplyAns = multiply(number1, number2);
+let divideAns = divide(number1, number2);
+
+
+window.alert(`${number1} + ${number2} = ${add(number1, number2)} \n
+${number1} - ${number2} = ${subtract(number1, number2)} \n
+${number1} * ${number2} = ${multiply(number1, number2)} \n
+${number1} / ${number2} = ${divide(number1, number2)}`);
